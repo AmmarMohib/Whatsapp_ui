b0a46c5f5e31b9ac2cda628dbfb419890f570e17
AmmarMohib <ammarmohib7000@gmail.com>
2022-04-02 11:14:18 +0500

    update

---
+++ lib/chats.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class Chats extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+              debugShowCheckedModeBanner: false,
+      home: Scaffold(
+
+        body:   
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage:
+                                NetworkImage('https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg'),
+                            backgroundColor: Colors.transparent,
+                          ),
+                          Text("Ali",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                      ],
+                    ),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage:
+                                NetworkImage('https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg'),
+                            backgroundColor: Colors.transparent,
+                      ),
+                      Text("Asad",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                          ],
+                        ),
+                                                  Row(
+                          children: [
+                            CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage:
+                                NetworkImage('https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg'),
+                            backgroundColor: Colors.transparent,
+                      ),
+                      Text("Qasim",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                          ],
+                        ),
+                                                Row(
+                          children: [
+                            CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage:
+                                NetworkImage('https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg'),
+                            backgroundColor: Colors.transparent,
+                      ),
+                      Text("asim",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                          ],
+                        ),              
+                                                Row(
+                          children: [
+                            CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage:
+                                NetworkImage('https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg'),
+                            backgroundColor: Colors.transparent,
+                      ),
+                      Text("doctor",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                          ],
+                        ),
+                                                Row(
+                          children: [
+                            CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage:
+                                NetworkImage('https://image.shutterstock.com/image-vector/man-character-face-avatar-glasses-260nw-562077406.jpg'),
+                            backgroundColor: Colors.transparent,
+                      ),
+                      Text("police",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                          ],
+                        ),
+                        ],
+                ),
+              ),
+            ),
+    );
+  }
+}
